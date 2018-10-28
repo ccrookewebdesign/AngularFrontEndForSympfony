a6ce580e0279137d435b7033e17eaabb14daeb06
Chris Crooke <chris@ccrooke.com>
2018-10-28 08:15:22 -1000

    Lots of changes

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,10 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  publicKey: 'ca97542893d7ec6e48bf0495dde7b8d1',
+  privateKey:'db9fc16ae8fa56b5280ca4c542a9683f0a7be260',
+  pageSize: 18
 };
 
 /*
